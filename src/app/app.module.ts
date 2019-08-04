65379e9d8ca738435d47eb901b9de0afeace1dd2
Nabeel73 <32979012+Nabeel73@users.noreply.github.com>
2019-08-04 13:14:18 +0500

    code push

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { CoursesComponent } from './courses.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { CourseComponent } from './course/course.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CourseComponent,
+    CoursesComponent
+
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
