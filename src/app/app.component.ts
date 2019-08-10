bf4b9dbb5f6dffc1455c175f189cb8c802964ef1
Nabeel73 <32979012+Nabeel73@users.noreply.github.com>
2019-08-10 12:33:16 +0500

    creating map view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  //title = 'Angular App';
-  courses  = [];
+  title = 'Angular App';
+  courses  = [1];
+  viewMode = 'map';
 }
